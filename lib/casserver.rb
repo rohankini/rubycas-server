445c7bf34c5d296ed4538b61fd2f0a7841a63401
Janmejay Singh <singh.janmejay@gmail.com>
2009-02-23 10:43:06 +0530

    Pulled picnic loading logic out to address duplication

--- lib/casserver.rb
+++ lib/casserver.rb
@@ -1,28 +1,8 @@
-unless Object.const_defined?(:Picnic)
-  $APP_NAME ||= 'rubycas-server'
-  $APP_ROOT ||= File.expand_path(File.dirname(__FILE__)+'/..')
-  
-  if File.exists?(picnic = File.expand_path(File.dirname(File.expand_path(__FILE__))+'/../vendor/picnic/lib'))
-    $: << picnic
-  elsif File.exists?(picnic = File.expand_path(File.dirname(File.expand_path(__FILE__))+'/../../picnic/lib'))
-    $: << picnic
-  else
-    require 'rubygems'
-    
-    # make things backwards-compatible for rubygems < 0.9.0
-    if respond_to?(:require_gem)
-      puts "WARNING: aliasing gem to require_gem in #{__FILE__} -- you should update your RubyGems system!"
-      alias gem require_gem
-    end
-   
-    gem 'picnic'
-  end
-  
-  require 'picnic'
-end
+require File.join(File.dirname(__FILE__), 'casserver', 'load_picnic')
 
 require 'yaml'
 require 'markaby'
+require 'activesupport'
 
 require "casserver/conf"
 require "picnic/logger"
@@ -46,7 +26,7 @@ require "casserver/localization"
 def CASServer.create
   CASServer::Models::Base.establish_connection($CONF[:database])
   CASServer::Models.create_schema
-  
+
   CASServer::Models::ServiceTicket.cleanup_expired($CONF[:service_ticket_expiry])
   CASServer::Models::LoginTicket.cleanup_expired($CONF[:login_ticket_expiry])
   CASServer::Models::ProxyGrantingTicket.cleanup_expired($CONF[:proxy_granting_ticket_expiry])
