238446054bbd10ede40ed8e2ad5cdcccef4c4dd9
Janmejay Singh <singh.janmejay@gmail.com>
2009-02-24 16:46:41 +0530

    changed the service to cas session validation to make it username based

--- lib/casserver/controllers.rb
+++ lib/casserver/controllers.rb
@@ -299,22 +299,20 @@ module CASServer::Controllers
   end
 
   # custom
-  class TgtValidate < R '/tgtValidate'
+  class SessionValidate < R '/sessionValidate'
     include CASServer::CAS
 
     def get
       CASServer::Utils::log_controller_action(self.class, @input)
 
       # required
-      tgt = @input['tgt']
+      username = @input['user']
 
-      tgt, @error = validate_ticket_granting_ticket(tgt)
-      @success = tgt && !@error
+      username, @error = validate_ticket_granting_ticket_for_user(username)
 
-      @username = tgt.username if @success
-
+      @success = username && !@error
+      @username = username if @success
       @status = response_status_from_error(@error) if @error
-
       render :validate
     end
   end
